56ac2dad9acdf16e9b82d30b0d7fb53ba466eb59
Allana Yzabelle M. Diaz <ayzabelled@gmail.com>
2025-02-19 16:59:21 +0800

    feat: added todo list to dashboard

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -2,8 +2,14 @@
 import { SessionProvider, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
-import { FinancialTracker, trackerColumns } from "@/components/columns"; // Import your type
+import {
+  FinancialTracker,
+  todolistColumn,
+  TodoListRequest,
+  trackerColumns,
+} from "@/components/columns"; // Import your type
 import { DahboardTable } from "@/components/dashboard-tables";
+import LoadingSpinner from "@/components/loading-indicator";
 
 const Dashboard: React.FC = () => {
   const { data: session, status } = useSession();
@@ -11,6 +17,7 @@ const Dashboard: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
   const [totalAmount, setTotalAmount] = useState(0);
   const [data, setData] = useState<FinancialTracker[] | null>(null);
+  const [todoData, setTodoData] = useState<TodoListRequest[] | null>(null);
 
   useEffect(() => {
     if (status === "loading") return;
@@ -43,7 +50,29 @@ const Dashboard: React.FC = () => {
         setData(result.data as FinancialTracker[]);
         setTotalAmount(result.totalAmount);
 
+        const todoResponse = await fetch(
+          `/api/to-do-list?userId=${session.user.id}`
+        );
+
+        if (!todoResponse.ok) {
+          const errorData = await todoResponse.json();
+          throw new Error(
+            errorData.error || `HTTP error! status: ${todoResponse.status}`
+          );
+        }
+
+        const todoResult = await todoResponse.json();
+        console.log("todolist: ", todoResult);
+
+        setTodoData(todoResult.data as TodoListRequest[]);
+
         if (!result.data || !Array.isArray(result.data)) {
+          throw new Error("Invalid data format received from the API.");
+        }
+
+        setTodoData(todoResult.data as TodoListRequest[]);
+
+        if (!todoResult.data || !Array.isArray(todoResult.data)) {
           throw new Error("Invalid data format received from the API.");
         }
       } catch (err) {
@@ -59,29 +88,28 @@ const Dashboard: React.FC = () => {
     fetchData();
   }, [status, session]);
 
+  const handleCheckboxChange = async (id: string) => {
+    try {
+      const response = await fetch(`/api/to-do-list`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, done: true }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update received status");
+      }
+      // Refresh the page after successful update
+      window.location.reload();
+    } catch (error) {
+      console.error("Error updating received status:", error);
+    }
+  };
+
   if (status === "loading") {
-    return <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
-    <svg
-      className="animate-spin size-10 text-[#6486DB]"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <circle
-        className="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="currentColor"
-        strokeWidth="4"
-      />
-      <path
-        className="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8v2a6 6 0 100 12v2a8 8 0 01-8-8z"
-      />
-    </svg>
-  </div>;
+    return <LoadingSpinner />;
   }
 
   if (status === "unauthenticated" || !session) {
@@ -95,51 +123,42 @@ const Dashboard: React.FC = () => {
   // User is authenticated, but no data is being fetched, so an empty dashboard
   return (
     <>
-      {loading && (
-        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
-          <svg
-            className="animate-spin size-10 text-[#6486DB]"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            />
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8v2a6 6 0 100 12v2a8 8 0 01-8-8z"
-            />
-          </svg>
-        </div>
-      )}
-      <div className="p-2 pl-8">
+      {loading && <LoadingSpinner />}
+      <div className="p-2">
         <span className="text-xl">Welcome back, </span>
         <span className="text-xl font-bold"> 🍡 {session?.user?.name} 🍡</span>
 
         <div>
           <h1 className="text-2xl font-bold text-white pt-2 pb-2">
-            Financial Tracker
+            Dashboard
           </h1>{" "}
           {/* Updated title */}
           <div className="bg-[#f4f4f4] rounded-xl shadow-md flex justify-center text-[#212121] text-xl font-bold h-9">
             <p className="flex items-center">₱{totalAmount}</p>
           </div>
           <p className="flex justify-center pt-1 italic text-xs">Balance</p>
-          <h1 className="text-xl font-bold text-white pt-2 pb-4">
+          <h1 className="text-xl font-bold text-white pt-2 pb-2">
             Recent Transactions
           </h1>{" "}
           <div>
             {data && <DahboardTable columns={trackerColumns} data={data} />}
           </div>
         </div>
-        <div></div>
+        <div>
+          <h1 className="text-xl font-bold text-white pt-4 pb-2">
+            {" "}
+            To-do List
+          </h1>
+        </div>
+        <div>
+          {todoData && (
+            <DahboardTable
+              columns={todolistColumn}
+              data={todoData}
+              onCheckboxChange={handleCheckboxChange}
+            />
+          )}
+        </div>
       </div>
     </>
   );
